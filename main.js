1521923f018a9d9656828230659dd12323961c04
Animemuically6@gamil.com <Animemuically6@gamil.com>
2023-02-08 17:28:48 +0530

    web-side-riddhi-siddhi

---
+++ main.js
@@ -0,0 +1,15 @@
+
+console.log('Hello World!');
+var sidemeu = document.getElementById('sidemenu');
+  
+  function openemenu(){
+    sidemenu.style.right ='0';
+  }
+  
+  function closemenu(){
+        sidemenu.style.right ='-200px';
+
+  }
+
+/*------------------------slider js----*/
+
